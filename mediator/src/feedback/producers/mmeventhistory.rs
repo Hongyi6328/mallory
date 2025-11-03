3375b4e7ba68ca3abb989cfb2ddde479eb26337d
Hongyi6328 <6328huang@gmail.com>
2025-11-03 21:11:01 +0800

    Add MMState diff ratio avg to MMEventHistoryStat

--- mediator/src/feedback/producers/mmeventhistory.rs
+++ mediator/src/feedback/producers/mmeventhistory.rs
@@ -46,6 +46,8 @@ pub struct MMEventHistoryStat {
     pub per_event_mm_states_diff_count: usize,
     pub global_mm_states_hash_hit_count: usize,
     pub per_event_mm_states_hash_hit_count: usize,
+    pub global_mm_states_diff_ratio_sum: f64,
+    pub per_event_mm_states_diff_ratio_sum: f64,
     pub global_mm_config: Arc<MMConfig>,
     pub per_event_mm_config: Arc<MMConfig>,
     pub similarity_config: Arc<SimilarityConfig>,
@@ -82,6 +84,8 @@ impl MMEventHistoryStat {
             per_event_mm_states_diff_count: 0,
             global_mm_states_hash_hit_count: 0,
             per_event_mm_states_hash_hit_count: 0,
+            global_mm_states_diff_ratio_sum: 0.0,
+            per_event_mm_states_diff_ratio_sum: 0.0,
             num_global_mm_states_since_last_reward: 0,
             num_per_event_mm_states_since_last_reward: 0,
             global_mm_config: global_mm_config.clone(),
@@ -233,9 +237,11 @@ impl MMEventHistoryStat {
                     if is_global_state {
                         self.global_mm_states_diff_sum += comparision_result.2 as usize;
                         self.global_mm_states_diff_count += 1;
+                        self.global_mm_states_diff_ratio_sum += comparision_result.3;
                     } else {
                         self.per_event_mm_states_diff_sum += comparision_result.2 as usize;
                         self.per_event_mm_states_diff_count += 1;
+                        self.per_event_mm_states_diff_ratio_sum += comparision_result.3;
                     }
                 };
                 comparision_result.0
@@ -264,8 +270,8 @@ impl MMEventHistoryStat {
         is_global_state: bool,
         mm_config: Arc<MMConfig>,
         similarity_config: Arc<SimilarityConfig>,
-    ) -> (bool, bool, u64) {
-        // (is_similar, skipped_diff, diff)
+    ) -> (bool, bool, u64, f64) {
+        // (is_similar, skipped_diff, diff, diff_ratio)
         let use_absolute_threshold = similarity_config.mm_use_absolute_similarity_threshold;
         let (vc_sum1, mm_sum1) = (state1.vc_sum as u64, state1.mm_sum as u64);
         let (vc_sum2, mm_sum2) = (state2.vc_sum as u64, state2.mm_sum as u64);
@@ -317,13 +323,14 @@ impl MMEventHistoryStat {
                 }
             };
             if vc_sum_min < (vc_sum_max - thres_vc) || mm_sum_min < (mm_sum_max - thres_mm) {
-                return (false, true, 0);
+                return (false, true, 0, 0.0);
             }
             let (diff_vc, diff_mm) = state1.diff_split(state2);
             return (
                 diff_vc <= thres_vc && diff_mm <= thres_mm,
                 false,
                 diff_vc + diff_mm,
+                (diff_vc + diff_mm) as f64 / (vc_sum_max + mm_sum_max) as f64,
             ); // HONGYI TODO: return both diffs?
         }
 
@@ -352,10 +359,10 @@ impl MMEventHistoryStat {
             }
         };
         if sum_min < (sum_max - thres) {
-            return (false, true, 0);
+            return (false, true, 0, 0.0);
         }
         let diff = state1.diff(state2);
-        (diff <= thres, false, diff)
+        (diff <= thres, false, diff, diff as f64 / sum_max as f64)
     }
 
     fn insert_record(val: usize, top_records: &mut Vec<usize>) {
@@ -398,12 +405,12 @@ impl MMEventHistoryStat {
     }
 
     pub fn report_reward(&mut self) -> f64 {
-        // TODO: customize reward function
+        // HONGYI TODO: customize reward function
 
         let cool_down_factor: f64 = if (self.mm_reward_config.use_cool_down_factor
-            && self.mm_reward_config.cool_down_threshold > self.overall_total_events)
+            && self.mm_reward_config.cool_down_threshold > self.overall_total_global_mm_states)
         {
-            (self.overall_total_events as f64).min(self.mm_reward_config.cool_down_threshold as f64)
+            (self.overall_total_global_mm_states as f64).min(self.mm_reward_config.cool_down_threshold as f64)
                 / self.mm_reward_config.cool_down_threshold as f64 // Not to favour early rewards too much
         } else {
             1.0
@@ -446,6 +453,13 @@ impl RewardFunction for MMEventHistoryStat {
 impl fmt::Display for MMEventHistoryStat {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(f, "MMEventHistory[")?;
+
+        // Global State Statistics
+        writeln!(
+            f,
+            "GLOBAL_STATE_STATISTICS:"
+        )?;
+
         writeln!(
             f,
             "overall_total_message_events / overall_total_events: {} / {}",
@@ -491,6 +505,21 @@ impl fmt::Display for MMEventHistoryStat {
 
         writeln!(
             f,
+            "global_mm_states_diff_ratio_avg: {:.4}",
+            if self.global_mm_states_diff_count > 0 {
+                self.global_mm_states_diff_ratio_sum / self.global_mm_states_diff_count as f64
+            } else {
+                0.0
+            }
+        )?;
+
+        // Per-Event State Statistics
+        writeln!(f,
+            "PER_EVENT_STATE_STATISTICS:"
+        )?;
+
+        writeln!(
+            f,
             "overall_per_node_message_events / overall_per_node_events: {:?} / {:?}",
             self.overall_per_node_message_events, self.overall_per_node_events,
         )?;
@@ -528,6 +557,16 @@ impl fmt::Display for MMEventHistoryStat {
             if self.per_event_mm_states_diff_count > 0 {
                 self.per_event_mm_states_diff_sum as f64
                     / self.per_event_mm_states_diff_count as f64
+            } else {
+                0.0
+            }
+        )?;
+
+        writeln!(
+            f,
+            "per_event_mm_states_diff_ratio_avg: {:.4}",
+            if self.per_event_mm_states_diff_count > 0 {
+                self.per_event_mm_states_diff_ratio_sum / self.per_event_mm_states_diff_count as f64
             } else {
                 0.0
             }
